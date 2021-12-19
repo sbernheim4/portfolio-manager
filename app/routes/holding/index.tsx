90c35906566dc73a6a14bd9490443f53aa8c24d1
sbernheim4 <sambernheim@gmail.com>
2021-12-18 23:17:23 -0500

    feat: fill in the holding route

--- app/routes/holding/index.tsx
+++ app/routes/holding/index.tsx
@@ -1,11 +1,66 @@
-const Positions = () => {
+import { AccountBase } from "plaid";
+import { json, LinksFunction, LoaderFunction, MetaFunction, useLoaderData } from "remix";
+import { Positions } from "~/components/Positions";
+import { isFilled } from "~/helpers/isFilled";
+import { getInvestmentHoldings, getPlaidAccountBalances } from "~/helpers/plaidUtils";
+import { InvestmentResponse } from '../../types/index';
+import holdingStyles from "./../../styles/holding.css"
+
+export const meta: MetaFunction = () => {
+	return {
+		title: "Your Investments",
+		description: "View your investments across your entire portfolio"
+	};
+};
+
+export const links: LinksFunction = () => {
+	return [
+		{ rel: "stylesheet", href: holdingStyles }
+	];
+};
+
+export const loader: LoaderFunction = async () => {
+
+	const promises: [
+		Promise<InvestmentResponse>,
+		Promise<Array<AccountBase>>
+	] = [
+		getInvestmentHoldings(),
+		getPlaidAccountBalances()
+	];
+
+	const results = await Promise.allSettled(promises);
+
+	// @ts-ignore
+	const resolvedPromises: [
+		InvestmentResponse,
+		AccountBase[]
+	] = results
+		// @ts-ignore
+		.filter(isFilled)
+		// @ts-ignore
+		.map(x => x.value);
+
+	const [investmentData, balances] = resolvedPromises;
+	const { holdings, securities } = investmentData;
+
+	return json(
+		{ balances, holdings, securities },
+		{ headers: { "Cache-Control": "max-age=43200" } }
+	);
+
+};
+
+const Holdings = () => {
+    const investmentData = useLoaderData<InvestmentResponse>();
+	const { holdings, securities } = investmentData;
 
 	return (
 		<>
-			<p>Add this later</p>
+			<Positions securities={securities} holdings={holdings} />
 		</>
 	);
 
 };
 
-export default Positions;
+export default Holdings;
