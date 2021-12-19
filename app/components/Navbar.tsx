13150391551d8b961432857151d9845664cfa1f3
sbernheim4 <sambernheim@gmail.com>
2021-12-18 23:18:16 -0500

    improvement: add link account to navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,6 +7,7 @@ export const Navbar = () => {
 			<Link to={"/dashboard"}>Dashboard</Link>
 			<Link to={"/account"}>Accounts</Link>
 			<Link to={"/holding"}>Positions</Link>
+			<Link to={"/link-account"}>Link Account</Link>
 		</nav>
 	)
 
