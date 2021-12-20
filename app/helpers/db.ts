f0a474d1c1ccfd3519a9578c1a2b22d61403814b
sbernheim4 <sambernheim@gmail.com>
2021-12-20 12:48:46 -0500

    fix: db

--- app/helpers/db.ts
+++ app/helpers/db.ts
@@ -1,6 +1,9 @@
 import { MongoClient } from 'mongodb';
 import { MONGODB_PWD } from "../../env";
 
+const userId = "sams_tokens";
+const collectionName = "keys";
+
 const uri = `mongodb+srv://portfolio-manager:${MONGODB_PWD}@cluster0.bvttm.mongodb.net/plaid?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
@@ -32,16 +35,21 @@ const getDBConnection = async () => {
 
 };
 
-const retrieveStoredAccessTokens = async (userId = "sams_token") => {
+const retrieveStoredAccessTokens = async () => {
 
-	const keysCollection = await getKeysCollection();
-	const existingTokens = await keysCollection.findOne({ user: userId });
+    try {
+        const keysCollection = await getKeysCollection();
+        const existingTokens = await keysCollection.findOne({ user: userId });
 
-	if (existingTokens === null) {
-		return [];
-	}
+        if (existingTokens === null) {
+            return [];
+        }
 
-	return existingTokens.accessTokens as Array<string>;
+        return existingTokens.accessTokens as Array<string>;
+    } catch (err) {
+        console.log("ERR", err);
+        return [];
+    }
 
 };
 
@@ -53,10 +61,10 @@ const getKeysCollection = async () => {
 		throw new Error("Could not connect to DB");
 	}
 
-	return connection.db().collection('keys')
+	return connection.db().collection(collectionName);
 };
 
-const saveNewAccessToken = async (accessToken: string, userId = "sams_tokens") => {
+const saveNewAccessToken = async (accessToken: string) => {
 
 	const keysCollection = await getKeysCollection();
 	const existingTokens = await keysCollection.findOne({ user: userId});
