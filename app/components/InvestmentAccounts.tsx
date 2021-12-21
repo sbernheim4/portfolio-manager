156b6e6f9a304c36a1017d6bf213be7fedd17917
sbernheim4 <sambernheim@gmail.com>
2021-12-20 20:32:17 -0500

    fix: check if value is undefined

--- app/components/InvestmentAccounts.tsx
+++ app/components/InvestmentAccounts.tsx
@@ -3,25 +3,25 @@ import { Link } from "remix";
 
 export const InvestmentAccounts = (props: {balances: AccountBase[], securities: Security[], holdings: Holding[]}) => {
 
-	const { balances, securities, holdings } = props;
+	const { balances } = props;
 
-	const foo = balances.filter((account) => {
+	const investmentAndBrokerageAccounts = balances.filter((account) => {
 		return account.type === "investment" || account.type === "brokerage"
 	});
 
 	return (
 		<>
-		<h1>Investment and Brokerage Accounts</h1>
+			<h1>Investment and Brokerage Accounts</h1>
 
-		{
-			foo.map((account) => {
-				return (
-					<Link key={account.account_id} to={`/account/${account.account_id}`}>
-						<p>{account.name}</p>
-					</Link>
-				);
-			})
-		}
+			{
+				investmentAndBrokerageAccounts.map((account) => {
+					return (
+						<Link key={account.account_id} to={`/account/${account.account_id}`}>
+							<p>{account.name}</p>
+						</Link>
+					);
+				})
+			}
 		</>
 	);
 
