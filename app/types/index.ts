df5c99d6f3c74f48a17f8d9f052b7cd24bbde963
sbernheim4 <sambernheim@gmail.com>
2021-12-28 16:04:39 -0500

    feat: begin calculating XIRR

--- app/types/index.ts
+++ app/types/index.ts
@@ -4,10 +4,10 @@ export type DashboardProps = {
 	balances: AccountBase[];
 	holdings: Holding[];
 	securities: Security[];
-}
+};
 
 export type InvestmentResponse = {
 	holdings: Holding[];
 	securities: Security[];
-}
+};
 
