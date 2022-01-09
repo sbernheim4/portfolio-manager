a0689c80118019d51fda7c34cc210a1e7aa26d19
sbernheim4 <sambernheim@gmail.com>
2022-01-09 00:26:28 -0500

    fix: remove login for now

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,18 +4,19 @@ import { validateUserIsLoggedIn } from "~/helpers/validateUserIsLoggedIn";
 
 
 export const loader: LoaderFunction = async ({ request }) => {
-	return await validateUserIsLoggedIn(request);
+    // return await validateUserIsLoggedIn(request);
+    return null;
 };
 
 export default () => {
 
-	return (
-		<div>
-			<h1>Welcome to the homepage</h1>
-			<Link to={'/link-account'}>Link an account</Link>
-			<br />
-			<Link to={'/dashboard'}>Go to the dashboard</Link>
-		</div>
-	);
+    return (
+        <div>
+            <h1>Welcome to the homepage</h1>
+            <Link to={'/link-account'}>Link an account</Link>
+            <br />
+            <Link to={'/dashboard'}>Go to the dashboard</Link>
+        </div>
+    );
 
 };
