cb01d7d49be60b4c1ed0092609bdc3fa28394e4a
sbernheim4 <sambernheim@gmail.com>
2022-01-13 00:18:46 -0500

    improvement: increase maxAge

--- app/helpers/session.ts
+++ app/helpers/session.ts
@@ -1,29 +1,33 @@
 import { createCookieSessionStorage } from "remix";
 
-const { getSession, commitSession, destroySession } = createCookieSessionStorage({
-    // a Cookie from `createCookie` or the CookieOptions to create one
-    cookie: {
-        name: "__session",
+// 1 year
+// 365 days/year * 24 hours/day * 60 min/hour * 60 sec/min
+const maxAge = 365 * 24 * 60 * 60;
 
-        // all of these are optional
-        expires: new Date(Date.now() + 60),
-        httpOnly: true,
-        maxAge: 60,
-        path: "/",
-        sameSite: "lax",
-        secrets: ["s3cret1"],
-        secure: true
-    }
+const { getSession, commitSession, destroySession } = createCookieSessionStorage({
+	// a Cookie from `createCookie` or the CookieOptions to create one
+	cookie: {
+		name: "__session",
+
+		// all of these are optional
+		expires: new Date(Date.now() + 60),
+		httpOnly: true,
+		maxAge,
+		path: "/",
+		sameSite: "lax",
+		secrets: ["s3cret1"],
+		secure: true
+	}
 });
 
 export { getSession, commitSession, destroySession };
 
 export const getUserNameFromSession = async (request: Request) => {
 
-    const session = await getSession(request.headers.get("Cookie"));
+	const session = await getSession(request.headers.get("Cookie"));
 
-    const username = session.get("userId") as string;
+	const username = session.get("userId") as string;
 
-    return username;
+	return username;
 
 };
