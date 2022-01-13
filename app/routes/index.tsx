10231de92abe65f4faa464471b41245eb182e8d5
sbernheim4 <sambernheim@gmail.com>
2022-01-13 00:01:05 -0500

    fix: broken link

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,22 +5,22 @@ import { isLoggedOut } from "./login";
 
 export const loader: LoaderFunction = async ({ request }) => {
 
-	if (await isLoggedOut(request)) {
-		return redirect("/login");
-	}
+    if (await isLoggedOut(request)) {
+        return redirect("/login");
+    }
 
-	return null;
+    return null;
 };
 
 export default () => {
 
-	return (
-		<div>
-			<h1>Welcome to the homepage</h1>
-			<Link to={'/link-account'}>Link an account</Link>
-			<br />
-			<Link to={'/dashboard'}>Go to the dashboard</Link>
-		</div>
-	);
+    return (
+        <div>
+            <h1>Welcome to the homepage</h1>
+            <Link to={'/manage-accounts'}>Link an account</Link>
+            <br />
+            <Link to={'/dashboard'}>Go to the dashboard</Link>
+        </div>
+    );
 
 };
