b7633aae105b82f4ca2016cd35ccbc88526d753c
sbernheim4 <sambernheim@gmail.com>
2022-05-15 18:28:54 -0400

    cleanup: move positions table to its own component

--- app/components/Positions/Positions.tsx
+++ app/components/Positions/Positions.tsx
@@ -2,12 +2,10 @@ import { Holding, Security } from 'plaid';
 import { LinksFunction } from "@remix-run/node";
 import positionsStyles from './styles/positions.css';
 import {
-	StockInvestmentSummary,
 	links as stockInvestmentSummaryStyles
 } from '~/components/StockInvestmentSummary/StockInvestmentSummary';
 import { StockPieChart, links as StockPieChartStyles } from './StockPieChart/StockPieChart';
-import { useSearchableList } from '~/hooks/useSearchHoldings';
-import { Option } from 'excoptional';
+import { PositionsTable } from './PositionsTable';
 
 export const links: LinksFunction = () => {
 	return [
@@ -109,18 +107,6 @@ export const Positions = (
 
 	const { securities, holdings } = props;
 
-	const tickerSymbolToSecurityId = constructTickerSymbolToSecurityId(
-		securities
-	);
-
-	// Curried for use in the useSearchHoldings hook
-	const toMatchedSecurityIds = (searchTerm: string) => Object.keys(
-		tickerSymbolToSecurityId
-	)
-		.map(ticker => ticker.toUpperCase()) // Normalize
-		.filter(ticker => ticker.includes(searchTerm)) // Filter
-		.map(ticker => tickerSymbolToSecurityId[ticker]); // convert tickers to security id
-
 	// Note: A single stock can be held in multiple accounts. Plaid
 	// (correctly) returns the stock per each account that holds it. This
 	// means that if the same stock is held in two accounts, it will appear
@@ -129,73 +115,20 @@ export const Positions = (
 	// To dedupe these, we use this helper aggregateHoldings function
 	const aggregatedHoldings = aggregateHoldings(holdings);
 
-	const [searchTerm, setSearchTerm, holdingsToDisplay] = useSearchableList(
-		aggregatedHoldings,
-		(searchTerm: string) => (holding: Holding) => toMatchedSecurityIds(searchTerm).includes(holding.security_id),
-	);
-
 	const securityIdToTickerSymbol = constructSecurityIdToTickerSymbol(
 		securities
 	);
-
-	const totalInvested = aggregatedHoldings.reduce((acc, curr) => acc + curr.institution_value, 0);
-
-	const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setSearchTerm(e.target.value.toUpperCase());
-	};
 
 	return (
 		<div className="positions">
 			<h1>Your Positions</h1>
 
-			<div className="positions__table">
-
-				<input
-					className="positions__table__search"
-					value={searchTerm}
-					onChange={(e) => handleSearch(e)}
-					name="search"
-					type="search"
-					placeholder="Search by ticker"
-				/>
-
-				<table className="investment-line-items">
-					<thead>
-						<tr>
-							<th>Ticker</th>
-							<th>Quantity</th>
-							<th>Percentage</th>
-							<th>Dollar Value</th>
-							<th>Above Threshold</th>
-						</tr>
-					</thead>
-					<tbody>
-						{
-							holdingsToDisplay
-								.sort((a, b) => b.institution_value - a.institution_value)
-								.map((holding, i) => {
-
-									const ticker = Option.of(securityIdToTickerSymbol[holding.security_id]);
-
-									return (
-										<StockInvestmentSummary
-											tickerOpt={ticker}
-											totalInvested={totalInvested}
-											holding={holding}
-											securityId={securities[i].security_id}
-										/>
-									);
-								})
-						}
-					</tbody>
-				</table>
-			</div>
+			<PositionsTable securities={securities} holdings={holdings}/>
 
 			<StockPieChart
 				securityIdToTickerSymbol={securityIdToTickerSymbol}
 				holdings={aggregatedHoldings}
 			/>
-
 		</div>
 	);
 };
