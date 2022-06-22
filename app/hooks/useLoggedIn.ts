723cc3e05966871bf3d3798846910b45c5925463
sbernheim4 <sambernheim@gmail.com>
2022-06-22 18:02:08 -0400

    docs: add example for server side handling of hook

--- app/hooks/useLoggedIn.ts
+++ app/hooks/useLoggedIn.ts
@@ -10,6 +10,21 @@ import { useEffect } from "react";
  *
  * Should be **the first hook called** by route components that display personal
  * data where the data is also cached.
+ *
+ * Ensure routes have an action function to handle the action type 'isLoggedIn'
+ * which simply run `await validateIsLoggedIn(request);`
+ *
+ * @example
+ * export const action: ActionFunction = async ({ request }) => {
+ *
+ *   const formData = await request.formData();
+ *   const action = formData.get("_action");
+ *
+ *   switch (action) {
+ *     case 'isLoggedIn':
+ *       await validateIsLoggedIn(request);
+ *   }
+ * }
  */
 export const useLoggedIn = () => {
 
