ba95e71e03335ea2509c6a13d6f52389696a296f
sbernheim4 <sambernheim@gmail.com>
2022-06-30 02:28:08 -0400

    improvement: begin making navbar login/logout route and text stateful

--- app/helpers/isLoggedOut.ts
+++ app/helpers/isLoggedOut.ts
@@ -3,6 +3,7 @@ import { getUserNameFromSession } from "./session";
 export const isLoggedOut = async (request: Request) => {
 
 	const userId = await getUserNameFromSession(request);
+	console.log({ userId })
 
 	if (userId === null || userId === undefined) {
 		return true;
