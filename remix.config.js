466b816c63acfee77585bc0af77bff5f1e5b2280
Sam Bernheim <sambernheim@icloud.com>
2023-12-08 03:10:31 -0500

    chore: update for vercel deployment

--- remix.config.js
+++ remix.config.js
@@ -7,7 +7,6 @@ export default {
 	appDirectory: "app",
 	assetsBuildDirectory: "public/build",
 	publicPath: "/build/",
-	serverBuildDirectory: "api/_build",
 	ignoredRouteFiles: [".*"],
 	routes(defineRoutes) {
 		// uses the v1 convention, works in v1.15+ and v2
